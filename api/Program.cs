e5528a0b92fcf1e1cf1ac52b9f68705b59f4ac72
markjameshoward@hotmail.co.uk <markjameshoward@hotmail.co.uk>
2026-01-30 12:43:43 +1300

    using tgitapi for azure web app now

--- api/Program.cs
+++ api/Program.cs
@@ -32,14 +32,11 @@ else
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "TGit API v1");
-    });
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "TGit API v1");
+});
 
 app.UseCors();
 
